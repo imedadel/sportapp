73d409538dd3e92311fcb1f7101d42044cbd2ad2
Imed Adel <aadelimed@gmail.com>
2019-12-06 06:04:06 +0100

    set sport page

---
+++ pages/events/index.js
@@ -0,0 +1,82 @@
+import React from "react"
+import "../styles/index.css"
+import { useRouter } from "next/router"
+import fetch from "isomorphic-unfetch"
+
+function Page({ sports }) {
+	const router = useRouter()
+	const { type, env, place, free, intensity } = router.query
+	const [filteredSports, setFilteredSports] = React.useState([])
+
+	function filterData(sport) {
+		return (
+			sport.relationships.tags.data.some(tag => tag.includes(env)) ||
+			sport.attributes.name.includes(env)
+		)
+	}
+
+	React.useEffect(() => {
+		setFilteredSports(sports.filter(filterData))
+	}, [sports])
+
+	return (
+		<main className="mx-auto container">
+			<div>
+				<h1 className="mt-12 mb-8 text-4xl sm:text-5xl font-bold leading-none sm:leading-tighter tracking-tighter sm:text-center">
+					Sports tagged "{env}"
+				</h1>
+			</div>
+			<div className="flex flex-wrap justify-between">
+				{filteredSports.length > 0 &&
+					filteredSports.map((sport, key) => (
+						<div
+							key={key}
+							className="w-64 rounded overflow-hidden shadow-lg mr-2 mb-4"
+						>
+							<img
+								className="w-full h-40"
+								src={
+									!!sport.relationships.images &&
+									sport.relationships.images.data &&
+									sport.relationships.images.data.length > 0
+										? sport.relationships.images.data[0].url
+										: `https://source.unsplash.com/500x250/?${sport.attributes.name}`
+								}
+								alt={sport.attributes.name}
+							/>
+							<div className="px-6 py-4">
+								<div className="font-bold text-xl mb-2">
+									{sport.attributes.name}
+								</div>
+								<p className="text-gray-700 text-base">
+									{sport.attributes.description}
+								</p>
+							</div>
+							{/* <div className="px-6 py-4">
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+									#photography
+								</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+									#travel
+								</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+									#winter
+								</span>
+							</div> */}
+						</div>
+					))}
+			</div>
+		</main>
+	)
+}
+
+Page.getInitialProps = async function() {
+	const res = await fetch("https://sports.api.decathlon.com/sports")
+	const data = await res.json()
+
+	return {
+		sports: data.data,
+	}
+}
+
+export default Page
--- pages/sports/[name].js
+++ pages/sports/[name].js
@@ -7,7 +7,18 @@ function Page(data) {
 	const { name } = router.query
 	const [title, setTitle] = React.useState(name)
 	const [description, setDescription] = React.useState("")
-	const [defis, setDefis] = React.useState([])
+	const [defis, setDefis] = React.useState([
+		{
+			name: "7-Day Streak",
+			description: "Maintain a 7-day streak doing this activity.",
+			prize: "$10 Coupon",
+		},
+		{
+			name: "14-Day Streak",
+			description: "Maintain a 14-day streak doing this activity.",
+			prize: "$20 Coupon",
+		},
+	])
 	const [events, setEvents] = React.useState([])
 	const [equip, setEquip] = React.useState([])
 
@@ -20,14 +31,29 @@ function Page(data) {
 
 	return (
 		<main className="mx-auto container">
-			<div>
+			<div className="max-w-2xl mx-auto">
 				<h1 className="mt-12 mb-8 text-4xl sm:text-5xl font-bold leading-none sm:leading-tighter tracking-tighter sm:text-center">
 					{title}
 				</h1>
 				<p>{description}</p>
 			</div>
-			<div>
-				{defis.length > 0 && defis.map((def, key) => <div>{def.title}</div>)}
+			<div className="mt-8 max-w-2xl mx-auto">
+				{defis.length > 0 &&
+					defis.map((def, key) => (
+						<div key={key} className="mb-4">
+							<h2 className="font-semibold">{def.name}</h2>
+							<p>{def.description}</p>
+							<p>Prize: {def.prize}</p>
+							<button
+								className="bg-500-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+								onClick={() => {
+									window.localStorage.setItem("currentMilestone", def.name)
+								}}
+							>
+								Register!
+							</button>
+						</div>
+					))}
 			</div>
 		</main>
 	)
